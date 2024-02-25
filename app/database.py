cba6d176257ed59ee1865030c097bde111361b7d
Shani Henriques <shanihenriques@gmail.com>
2024-02-25 20:39:26 +0000

    Updated requirements and fixed database connection

--- app/database.py
+++ app/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from . models import Base
 
 
 DATABASE_URL = "sqlite:///../users.db"
 
 engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(bind=engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
