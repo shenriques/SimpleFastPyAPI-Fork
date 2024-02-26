e2f62c404968b5e46438d55fc04e94582ddc1e14
Shani Henriques <shanihenriques@gmail.com>
2024-02-26 17:41:11 +0000

    Removed CRUD endpoints for books from main and included a router to them

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models import User, Book
-from app.schema import UserCreate, UserUpdate, BookCreate, BookUpdate, BookShow
-from typing import List
+from app.models import User
+from app.schema import UserCreate, UserUpdate
+from app.routes import book
 
 
 app = FastAPI()
-
+app.include_router(book.router)
 
 @app.get("/users/")
 def get_all_users(db: Session = Depends(get_db)):
@@ -47,46 +47,3 @@ def delete_user_by_email(user_id: int, db: Session = Depends(get_db)):
     db.delete(db_user)
     db.commit()
     return {"message": "User deleted successfully"}
-
-@app.get("/books", response_model=List[BookShow])
-def get_all_books(db: Session = Depends(get_db)):
-    book_objects = db.query(Book).all() # obtain a list of all books in database
-    if (len(book_objects) == 0): # if the list is empty, raise a 404 error
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No books in database.")
-    books = [BookShow(title=book.title, author=book.author) for book in book_objects] # create a BookShow object for each book in list
-    return books
-
-@app.get("/books/{book_id}", response_model=BookShow)
-def get_book(book_id: int, db: Session = Depends(get_db)):
-    book = db.query(Book).filter(Book.id == book_id).first() # search for the book by ID
-    if not book: # raise a 404 error if book of that ID doesnt exist
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book {book_id} not found.")
-    open_book = BookShow(title=book.title, author=book.author) # create a BookShow object for the searched book
-    return open_book
-
-@app.post("/books")
-def create_book(book: BookCreate, db: Session = Depends(get_db)):
-    new_book = Book(**book.dict()) # unpack the request rather than manually assigning
-    db.add(new_book)
-    db.commit() # add new book to database
-    db.refresh(new_book) # retrieve the newly created book
-    return new_book
-
-@app.put("/books/{book_id}")
-def update_book(book_id: int, book: BookUpdate, db: Session = Depends(get_db)):
-    updated_book = db.query(Book).filter(Book.id == book_id).first()
-    if not updated_book:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book {book_id} not found.")
-    updated_book.title = book.title
-    updated_book.author = book.author
-    db.commit()
-    return {"message": "Book updated successfully"}
-
-@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_book(book_id: int, db: Session = Depends(get_db)):
-    deleted_book = db.query(Book).filter(Book.id == book_id).first()
-    if not deleted_book:
-        HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book {book_id} not found.")
-    db.delete(deleted_book)
-    db.commit()
-    return {"message": "Book deleted successfully"}
