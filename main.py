535144d07fef50c7338643ba9e10679ad195bf9f
Shani Henriques <shanihenriques@gmail.com>
2024-02-26 17:07:52 +0000

    Improved error handling using Fast APIs status codes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import User, Book
@@ -50,14 +50,17 @@ def delete_user_by_email(user_id: int, db: Session = Depends(get_db)):
 
 @app.get("/books")
 def get_all_books(db: Session = Depends(get_db)):
-    return db.query(Book).all()
+    books = db.query(Book).all()
+    if (len(books) == 0):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No books in database.")
+    return books
 
 @app.get("/books/{book_id}")
 def get_book(book_id: int, db: Session = Depends(get_db)):
     book = db.query(Book).filter(Book.id == book_id).first()
-    if book:
-        return book
-    raise HTTPException(status_code=404, detail="Book not found")
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book {book_id} not found.")
+    return book
 
 @app.post("/books")
 def create_book(book: BookCreate, db: Session = Depends(get_db)):
@@ -65,13 +68,13 @@ def create_book(book: BookCreate, db: Session = Depends(get_db)):
     db.add(new_book)
     db.commit()
     db.refresh(new_book)
-    return book
+    return new_book
 
 @app.put("/books/{book_id}")
 def update_book(book_id: int, book: BookUpdate, db: Session = Depends(get_db)):
     updated_book = db.query(Book).filter(Book.id == book_id).first()
     if not updated_book:
-        raise HTTPException(status_code=404, detail="Book not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book {book_id} not found.")
     updated_book.title = book.title
     updated_book.author = book.author
     db.commit()
@@ -81,7 +84,7 @@ def update_book(book_id: int, book: BookUpdate, db: Session = Depends(get_db)):
 def delete_book(book_id: int, db: Session = Depends(get_db)):
     deleted_book = db.query(Book).filter(Book.id == book_id).first()
     if not deleted_book:
-        raise HTTPException(status_code=404, detail="Book not found")
+        HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book {book_id} not found.")
     db.delete(deleted_book)
     db.commit()
     return {"message": "Book deleted successfully"}
